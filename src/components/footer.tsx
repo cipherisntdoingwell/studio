762108118e724e23cabfc63a448e06780a1cd5b3
Mouad Aqejtaoui <aqejtawitom123@gmail.com>
2025-10-06 17:30:53 +0000

    make the outline visible and round around the section items not the sect

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="border-t border-border/40">
+    <footer className="w-full py-6">
       <div className="container mx-auto flex h-14 max-w-screen-2xl items-center justify-center px-4 text-center text-sm text-muted-foreground md:px-6">
         <p>&copy; {currentYear} Cipheron. All rights reserved.</p>
       </div>
